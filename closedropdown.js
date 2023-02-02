c516d0b13c7263fc28e167cadbfbfc9a15742cf7
Sergiu Preguza <108341707+SergiuPreguza@users.noreply.github.com>
2023-02-02 15:42:22 +0200

    Add files via upload

---
+++ closedropdown.js
@@ -0,0 +1,7 @@
+var dropdown = document.getElementById('dropdown');
+var input = document.getElementById('input');
+window.addEventListener("click", function(event) {
+	if (event.target != dropdown && event.target != input) {
+		aux.style.display="none";
+	};
+});
